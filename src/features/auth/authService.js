4389e09a6e9cff4e57f90275dbb6b9e08c415ef9
martafralves <74558958+martafralves@users.noreply.github.com>
2022-12-08 22:35:35 +0000

    added backend url to user routes

--- src/features/auth/authService.js
+++ src/features/auth/authService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-
-const API_URL = '/api/users/'
+const SERVER_URL = process.env.REACT_APP_SERVER_URL
+const API_URL = `${SERVER_URL}+'/api/users/'`
 
 //Register user
 const registerUser = async(userData) => {
@@ -38,7 +38,7 @@ const updateUser = async(userId, token, updatedUser) => {
     }
 
     const response = await axios.put(API_URL + userId, config, updatedUser)
-    
+
     return response.data
 }
 
